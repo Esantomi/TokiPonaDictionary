7bbaf607a39642cddc155ab457dc0bce92b4484c
Esantomi <pilinnasa@gmail.com>
2021-05-19 18:46:47 +0900

    making GUI

---
+++ csv_adjusting.py
@@ -0,0 +1,60 @@
+# 긁어 온 파일 '단어', '뜻' 순서로 리스트에 넣기
+f = open('words.csv', 'rt', encoding = 'utf-8')
+
+nimi = []
+pilin = []
+
+for line in f:
+    # print(line)
+    if line[0] in 'aeioumnptkswlj':
+        nimi.append(line.strip())
+    else:
+        pilin.append(line.strip())
+
+lipu_pona = []
+for i, j in zip(nimi, pilin):
+    lipu_pona.append(i)
+    lipu_pona.append(j)
+
+# print(lipu_pona)
+
+
+# csv 형식에 맞는 파일 생성
+import csv
+
+with open('words_ordered.csv', 'w') as new_f:
+    writer = csv.DictWriter(new_f, fieldnames = ["단어", "뜻"])
+    writer.writeheader()
+    for line in lipu_pona:
+        # print(line)
+        if ',' in line:
+            a = line.replace(',', '/')
+            if a[0] in 'aeioumnptkswlj':
+                new_f.write(a + ',')
+            else:
+                new_f.write(a + '\n')
+        else:
+            if line[0] in 'aeioumnptkswlj':
+                new_f.write(line + ',')
+            else:
+                new_f.write(line + '\n')
+
+f.close()
+
+
+# 전체 단어 목록
+# lipu_nimi = {'a':'감탄사 전반',\
+#     'akesi':'파충류, 양서류, 징그러운 동물',\
+#     'ala':'부정어, 아니, 영(0)',\
+#     'alasa':'모으다, 사냥하다',\
+#     'ale':'모든, 만물, 우주, 인생',\
+#     'ali':'모든, 만물, 우주, 인생',\
+#     'anpa':'아래, 바닥',\
+#     'ante':'다른, 바꾸다'
+#     'anu':'또는'
+#     'awen':'남다, 머물다, 기다리다, 지키다',\
+#     'e':'직접 목적어를 받는 단어',\
+#     'en':'그리고',\
+#     }
+
+# print(lipu_nimi['a'])
--- toki_pona_dict.py
+++ toki_pona_dict.py
@@ -1,96 +1,59 @@
-# 긁어 온 파일 '단어', '뜻' 순서로 리스트에 넣기
-f = open('words.csv', 'rt', encoding = 'utf-8')
-
-nimi = []
-pilin = []
-
-for line in f:
-    # print(line)
-    if line[0] in 'aeioumnptkswlj':
-        nimi.append(line.strip())
-    else:
-        pilin.append(line.strip())
-
-lipu_pona = []
-for i, j in zip(nimi, pilin):
-    lipu_pona.append(i)
-    lipu_pona.append(j)
-
-# print(lipu_pona)
-
-
-# csv 형식에 맞는 파일 생성
-import csv
-
-with open('words_ordered.csv', 'w') as new_f:
-    writer = csv.DictWriter(new_f, fieldnames = ["단어", "뜻"])
-    writer.writeheader()
-    for line in lipu_pona:
-        # print(line)
-        if ',' in line:
-            a = line.replace(',', '/')
-            if a[0] in 'aeioumnptkswlj':
-                new_f.write(a + ',')
-            else:
-                new_f.write(a + '\n')
-        else:
-            if line[0] in 'aeioumnptkswlj':
-                new_f.write(line + ',')
-            else:
-                new_f.write(line + '\n')
-
-f.close()
-
-
 # 입력받아 뜻 찾아주기
 import csv
 
-with open('words_ordered.csv') as csv_dict:
-    reader = csv.DictReader(csv_dict)
-    lukin = input("단어를 입력하세요 : ")
-    for row in reader:
-        # print(row)
-        if lukin == row.get('단어'):
-            print(row.get('뜻'))
-        else:
-            continue
+def searchDict(lukin):
+    with open('words_ordered.csv') as csv_dict:
+        reader = csv.DictReader(csv_dict)
+        # lukin = input("단어를 입력하세요 : ")
+        for row in reader:
+            # print(row)
+            if lukin == row.get('단어'):
+                return print(row.get('뜻'))
+            else:
+                continue
+
+# searchDict('mun')
 
 
 # PyQt5
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QDesktopWidget
+from PyQt5.QtGui import QIcon
 
-class MyApp(QWidget):
+class TokiPonaDictApp(QWidget):
 
+    # 생성자 설정
     def __init__(self):
         super().__init__()
         self.initUI()
 
+    # UI 초기 설정
     def initUI(self):
+        self.lbl = QLabel(self)
+        self.lbl.move(60, 40)
+
+        qle = QLineEdit(self)
+        qle.move(60, 100)
+        qle.textChanged[str].connect(self.onChanged)
+
         self.setWindowTitle('한국어-도기 보나 사전')
-        self.move(300, 300)
-        self.resize(400, 200)
+        self.setWindowIcon(QIcon('icon.png'))
+        self.setGeometry(300, 300, 300, 200)
+        self.center()
         self.show()
+    
+    # 가운데 정렬
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def onChanged(self, text):
+        self.lbl.setText(searchDict(text))
+        self.lbl.adjustSize()
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
-
-
-# 전체 단어 목록
-# lipu_nimi = {'a':'감탄사 전반',\
-#     'akesi':'파충류, 양서류, 징그러운 동물',\
-#     'ala':'부정어, 아니, 영(0)',\
-#     'alasa':'모으다, 사냥하다',\
-#     'ale':'모든, 만물, 우주, 인생',\
-#     'ali':'모든, 만물, 우주, 인생',\
-#     'anpa':'아래, 바닥',\
-#     'ante':'다른, 바꾸다'
-#     'anu':'또는'
-#     'awen':'남다, 머물다, 기다리다, 지키다',\
-#     'e':'직접 목적어를 받는 단어',\
-#     'en':'그리고',\
-#     }
-
-# print(lipu_nimi['a'])
+    app = QApplication(sys.argv)
+    ex = TokiPonaDictApp()
+    sys.exit(app.exec_())
